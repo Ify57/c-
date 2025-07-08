2d13e9cc196ece65e38ff70fc55deeecf3dfcfc1
ifeanyi oleforo <ole24001@byui.edu>
2025-07-08 18:25:48 +0100

    assignment

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -8,3 +8,32 @@ Console.WriteLine(num);
 Console.WriteLine("whats ur name");
 String name = Console.ReadLine();
 Console.WriteLine("hello" +" "+ name);
+
+
+// condtional staement
+
+int x = 20;
+if (x < 20)
+{
+    Console.WriteLine("whats ur name");
+}
+
+
+// loops
+while (x > 20)
+{
+
+}
+
+// List
+List<int> ages = new List<int>();
+ages.Add(10);
+ages.Add(11);
+ages.Add(12);
+
+
+// method/function
+static void MyMethod()
+{
+    // code to be executed
+}
--- ConsoleApp1/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+144c49930a794c0f8a552956aa1965d9e40e5e43")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
