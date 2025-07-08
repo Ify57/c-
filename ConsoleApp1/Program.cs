144c49930a794c0f8a552956aa1965d9e40e5e43
ifeanyi oleforo <ole24001@byui.edu>
2025-07-08 13:52:14 +0100

    first commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+// variable
+int num = 10;
+Console.WriteLine(num);
+
+// input
+
+Console.WriteLine("whats ur name");
+String name = Console.ReadLine();
+Console.WriteLine("hello" +" "+ name);
