0f97cc7b53b7f4d370920c224ce67e3180a7899f
ifeanyi oleforo <ole24001@byui.edu>
2025-07-09 01:21:12 +0100

    more infor added

--- ConsoleApp1/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+144c49930a794c0f8a552956aa1965d9e40e5e43")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d13e9cc196ece65e38ff70fc55deeecf3dfcfc1")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
